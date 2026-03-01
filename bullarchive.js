027b8bc95dffd456e897b7868713c3e6bbb53781
sam-aum <psaum01@gmail.com>
2026-03-01 11:15:15 -0600

    added Nav files and moved Nav to the top

---
+++ bullarchive.js
@@ -0,0 +1,26 @@
+ document.write("<center> \
+ <a href=\"/bulletin/bulletin.html\"> 1998 - 2002</a> \&nbsp; \
+ <a href=\"/2003/2003b/2003bull.html\"> 2003</a> \&nbsp; \
+ <a href=\"/2004/2004b/2004bull.html\"> 2004</a> \&nbsp; \
+ <a href=\"/2005/2005b/2005bull.html\"> 2005</a> \&nbsp; \
+ <a href=\"/2006/2006b/2006bull.html\"> 2006</a> \&nbsp; \
+ <a href=\"/2007/2007b/2007bull.html\"> 2007</a> \&nbsp; \
+ <a href=\"/2008/2008b/2008bull.html\"> 2008</a> \&nbsp; \
+ <a href=\"/2009/2009b/2009bull.html\"> 2009</a> \&nbsp; \
+ <a href=\"/2010/2010b/2010bull.html\"> 2010</a> \&nbsp; \
+ <a href=\"/2011/2011b/2011bull.html\"> 2011</a> \&nbsp; \
+ <a href=\"/2012/2012b/2012bull.html\"> 2012</a> \&nbsp; \
+ <a href=\"/2013/2013b/2013bull.html\"> 2013</a> \&nbsp; \
+ <a href=\"/2014/2014b/2014bull.html\"> 2014</a> \&nbsp; \
+ <a href=\"/2015/2015b/2015bull.html\"> 2015</a> \&nbsp; \
+ <a href=\"/2016/2016b/2016bull.html\"> 2016</a> \&nbsp; \
+ <a href=\"/2017/2017b/2017bull.html\"> 2017</a> \&nbsp; \
+ <a href=\"/2018/2018b/2018bull.html\"> 2018</a> \&nbsp; \
+ <a href=\"/2019/2019b/2019bull.html\"> 2019</a> \&nbsp; \
+ <a href=\"/2020/2020b/2020bull.html\"> 2020</a> \&nbsp; \
+ <a href=\"/2021/2021b/2021bull.html\"> 2021</a> \&nbsp; \
+ <a href=\"/2022/2022b/2022bull.html\"> 2022</a> \&nbsp; \
+ <a href=\"/2023/2023b/2023bull.html\"> 2023</a> \&nbsp; \
+ <a href=\"/2024/2024b/2024bull.html\"> 2024</a> \&nbsp; \
+ <a href=\"/2025/2025b/2025bull.html\"> 2025</a> \&nbsp; \
+     </center>");
