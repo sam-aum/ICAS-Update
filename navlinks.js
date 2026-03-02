74c09f716d0e257bd7c2416dae700a7152bb06be
sam-aum <psaum01@gmail.com>
2026-03-01 21:07:07 -0600

    added test files

--- navlinks.js
+++ navlinks.js
@@ -1,28 +1,48 @@
- document.write(
-  "<center> \
-      <a href=\"/bullbase.html\"> Bulletins</a> \&nbsp; \
-      <a href=\"/contact.html\"> Contact</a> \&nbsp; \
-      <a href=\"/speakers.html\"> Contributors&nbsp;&#38;&nbsp;Speakers</a> \&nbsp; \
-      <a href=\"/delegate/delegate.html\"> Delegations</a> \&nbsp; \
-      <a href=\"/roster.html\">Fellows</a> \&nbsp; \
-      <a href=\"/golf/golf.html\">Golf</a> \&nbsp; \
-      <a href=\"/history/history.html\">Historical&nbsp;Documents</a> \&nbsp; \
-      <a href=\"/index.html\">Home</a> \&nbsp; \
-      <a href=\"/organize.html\">Index&nbsp;of&nbsp;Organizations</a> \&nbsp; \
-      <a href=\"/support.html\">Institutional&nbsp;Supporters</a> \&nbsp; \
-      <a href=\"/interns.html\">Interns&nbsp;&#38;&nbsp;Associates</a> \&nbsp;  \
-      <a href=\"/lectures.html\">Lectures</a> \&nbsp; \
-      <a href=\"/liberty.html\">Liberty&nbsp;Award</a> \&nbsp; \
-      <a href=\"/mission.html\">Mission</a> \&nbsp; \
-      <a href=\"/gallery/gallery.html\">Photos</a> \&nbsp; \
-      <a href=\"/archives/progarch.html\">Programs&nbsp;Archives</a> \&nbsp; \
-      <a href=\"/special.html\">Specials</a> \&nbsp; \
-      <a href=\"/staff.html\">Staff</a> \&nbsp; \
-      <a href=\"/strategy.html\">Strategy</a> \&nbsp; \
-      <a href=\"/symposia.html\">Symposia</a> \&nbsp; \
-      <a href=\"/tennis/tennis.html\">Tennis</a> \&nbsp; \
-      <a href=\"/upcome.html\">Upcoming</a> \&nbsp; \
-      <a href=\"/vidbase.html\">Videos</a> \&nbsp; \
-      <a href=\"/youth/yacademy.html\">Youth&nbsp;Academy</a>&nbsp; \
-  </center>"
-);
+document.addEventListener("DOMContentLoaded", function () {
+
+  const nav = document.getElementById("main-nav");
+
+  const links = [
+      { href: "/bullbase.html", label: "Bulletins" },
+      { href: "/contact.html", label: "Contact" },
+      { href: "/speakers.html", label: "Contributors & Speakers" },
+      { href: "/delegate/delegate.html", label: "Delegations" },
+      { href: "/roster.html", label: "Fellows" },
+      { href: "/golf/golf.html", label: "Golf" },
+      { href: "/history/history.html", label: "Historical Documents" },
+      { href: "/index.html", label: "Home" },
+      { href: "/organize.html", label: "Index of Organizations" },
+      { href: "/support.html", label: "Institutional Supporters" },
+      { href: "/interns.html", label: "Interns & Associates" },
+      { href: "/lectures.html", label: "Lectures" },
+      { href: "/liberty.html", label: "Liberty Award" },
+      { href: "/mission.html", label: "Mission" },
+      { href: "/gallery/gallery.html", label: "Photos" },
+      { href: "/archives/progarch.html", label: "Programs Archives" },
+      { href: "/special.html", label: "Specials" },
+      { href: "/staff.html", label: "Staff" },
+      { href: "/strategy.html", label: "Strategy" },
+      { href: "/symposia.html", label: "Symposia" },
+      { href: "/tennis/tennis.html", label: "Tennis" },
+      { href: "/upcome.html", label: "Upcoming" },
+      { href: "/vidbase.html", label: "Videos" },
+      { href: "/youth/yacademy.html", label: "Youth Academy" }
+  ];
+
+  const ul = document.createElement("ul");
+  ul.classList.add("nav-list");
+
+  links.forEach(link => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+
+      a.href = link.href;
+      a.textContent = link.label;
+
+      li.appendChild(a);
+      ul.appendChild(li);
+  });
+
+  nav.appendChild(ul);
+
+});
