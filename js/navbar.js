9fe0b6577563012014179d751c7d1de13d6f7cf1
sam-aum <psaum01@gmail.com>
2026-03-03 13:55:35 -0600

    organized files into folders

--- js/navbar.js
+++ js/navbar.js
@@ -2,8 +2,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navHost = document.getElementById("navigate");
     if (!navHost) return;
-  
-
 
     const nav = {
       top: [
@@ -83,7 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
       <nav class="main-nav" aria-label="Primary">
         <div class="nav-inner">
             <a class="nav-brand" href="/index.html">
-                <img src="/ICAS-pro-logo-1.png" alt="ICAS Home">
+                <img src="images/icas-logo.png" alt="ICAS Home">
             </a>
   
           <div id="primary-nav" class="nav-links">
