21ecd780702ab2059b51769ad02f96c6f2331345
sam-aum <psaum01@gmail.com>
2026-03-16 22:41:58 -0500

    fixed bulletin header

--- sandbox/js/bullhead.js
+++ sandbox/js/bullhead.js
@@ -1,5 +1,18 @@
-document.write("<tr><td align=\"center\"> \
-  <span class=\"B1\">ICAS </span><span class=\"B2\">Bulletin</span><br> \
-  <hr width=\"450\" noshade align=\"center\"> \
-  <span class=\"B3\">Institute for Corean-American Studies, Inc.</span><br><br> \
-   </td></tr>");
+document.addEventListener("DOMContentLoaded", () => {
+  const main = document.querySelector(".bulletin-card, #main");
+  if (!main) return;
+
+  const header = document.createElement("div");
+  header.className = "bulletin-topmark";
+  header.innerHTML = `
+    <div class="bulletin-topmark-inner">
+      <p class="bulletin-label">
+        <span class="B1">ICAS </span><span class="B2">Bulletin</span>
+      </p>
+      <hr class="bulletin-rule">
+      <p class="bulletin-org">Institute for Corean-American Studies, Inc.</p>
+    </div>
+  `;
+
+  main.prepend(header);
+});
